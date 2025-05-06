169d89acecb678542e9d62e76dd086cc3e120930
lualon09 <lualon09@ucm.es>
2025-05-06 13:15:02 +0200

    DMP power consumption formatted

--- ICM_20948/Power_consumption_DMP/Power_consumption_DMP.ino
+++ ICM_20948/Power_consumption_DMP/Power_consumption_DMP.ino
@@ -1,239 +1,4 @@
 /****************************************************************
- * Example7_DMP_Quat6_EulerAngles.ino
- * ICM 20948 Arduino Library Demo
- * Initialize the DMP based on the TDK InvenSense ICM20948_eMD_nucleo_1.0 example-icm20948
- * Paul Clark, April 25th, 2021
- * Based on original code by:
- * Owen Lyke @ SparkFun Electronics
- * Original Creation Date: April 17 2019
- * 
- * ** This example is based on InvenSense's _confidential_ Application Note "Programming Sequence for DMP Hardware Functions".
- * ** We are grateful to InvenSense for sharing this with us.
- * 
- * ** Important note: by default the DMP functionality is disabled in the library
- * ** as the DMP firmware takes up 14301 Bytes of program memory.
- * ** To use the DMP, you will need to:
- * ** Edit ICM_20948_C.h
- * ** Uncomment line 29: #define ICM_20948_USE_DMP
- * ** Save changes
- * ** If you are using Windows, you can find ICM_20948_C.h in:
- * ** Documents\Arduino\libraries\SparkFun_ICM-20948_ArduinoLibrary\src\util
- *
- * Please see License.md for the license information.
- *
- * Distributed as-is; no warranty is given.
- ***************************************************************/
-
-//#define QUAT_ANIMATION // Uncomment this line to output data in the correct format for ZaneL's Node.js Quaternion animation tool: https://github.com/ZaneL/quaternion_sensor_3d_nodejs
-
-/*
-#include "ICM_20948.h" // Click here to get the library: http://librarymanager/All#SparkFun_ICM_20948_IMU
-#include <BMP388_DEV.h> 
-
-//#define USE_SPI       // Uncomment this to use SPI
-
-#define SERIAL_PORT Serial
-
-#define SPI_PORT SPI // Your desired SPI port.       Used only when "USE_SPI" is defined
-#define CS_PIN 2     // Which pin you connect CS to. Used only when "USE_SPI" is defined
-
-// Pines a añadir
-#define SCL_PIN 6
-#define SDA_PIN 7
-#define REG_1V8_EN_PIN 8 //enable 1v8 register
-
-#define WIRE_PORT Wire // Your desired Wire port.      Used when "USE_SPI" is not defined
-// The value of the last bit of the I2C address.
-// On the SparkFun 9DoF IMU breakout the default is 1, and when the ADR jumper is closed the value becomes 0
-#define AD0_VAL 0
-
-ICM_20948_I2C myICM; // Otherwise create an ICM_20948_I2C object
-
-void setup()
-{
-
-  delay(100);
-
-  // SERIAL_PORT.println(F("Press any key to continue..."));
-
-  // while (!SERIAL_PORT.available()) // Wait for the user to press a key (send any serial character)
-  //  ;
-
-  WIRE_PORT.setPins(SDA_PIN,SCL_PIN); 
-  pinMode(REG_1V8_EN_PIN, OUTPUT);
-  digitalWrite(REG_1V8_EN_PIN, 1);
-  delay(100);
-  WIRE_PORT.begin();
-  WIRE_PORT.setClock(400000);
-  BMP388_DEV bmp388;   
-  bmp388.begin(SLEEP_MODE, 0x76);
-
-  bool initialized = false;
-  while (!initialized)
-  {
-
-    // Initialize the ICM-20948
-    // If the DMP is enabled, .begin performs a minimal startup. We need to configure the sample mode etc. manually.
-    myICM.begin(WIRE_PORT, AD0_VAL);
-    if (myICM.status != ICM_20948_Stat_Ok)
-    {
-      delay(500);
-    }
-    else
-    {
-      initialized = true;
-    }
-  }
-
-  bool success = true; // Use success to show if the DMP configuration was successful
-
-  // Initialize the DMP. initializeDMP is a weak function. You can overwrite it if you want to e.g. to change the sample rate
-  success &= (myICM.initializeDMP() == ICM_20948_Stat_Ok);
-
-  // DMP sensor options are defined in ICM_20948_DMP.h
-  //    INV_ICM20948_SENSOR_ACCELEROMETER               (16-bit accel)
-  //    INV_ICM20948_SENSOR_GYROSCOPE                   (16-bit gyro + 32-bit calibrated gyro)
-  //    INV_ICM20948_SENSOR_RAW_ACCELEROMETER           (16-bit accel)
-  //    INV_ICM20948_SENSOR_RAW_GYROSCOPE               (16-bit gyro + 32-bit calibrated gyro)
-  //    INV_ICM20948_SENSOR_MAGNETIC_FIELD_UNCALIBRATED (16-bit compass)
-  //    INV_ICM20948_SENSOR_GYROSCOPE_UNCALIBRATED      (16-bit gyro)
-  //    INV_ICM20948_SENSOR_STEP_DETECTOR               (Pedometer Step Detector)
-  //    INV_ICM20948_SENSOR_STEP_COUNTER                (Pedometer Step Detector)
-  //    INV_ICM20948_SENSOR_GAME_ROTATION_VECTOR        (32-bit 6-axis quaternion)
-  //    INV_ICM20948_SENSOR_ROTATION_VECTOR             (32-bit 9-axis quaternion + heading accuracy)
-  //    INV_ICM20948_SENSOR_GEOMAGNETIC_ROTATION_VECTOR (32-bit Geomag RV + heading accuracy)
-  //    INV_ICM20948_SENSOR_GEOMAGNETIC_FIELD           (32-bit calibrated compass)
-  //    INV_ICM20948_SENSOR_GRAVITY                     (32-bit 6-axis quaternion)
-  //    INV_ICM20948_SENSOR_LINEAR_ACCELERATION         (16-bit accel + 32-bit 6-axis quaternion)
-  //    INV_ICM20948_SENSOR_ORIENTATION                 (32-bit 9-axis quaternion + heading accuracy)
-
-  // Enable the DMP Game Rotation Vector sensor
-  success &= (myICM.enableDMPSensor(INV_ICM20948_SENSOR_GAME_ROTATION_VECTOR) == ICM_20948_Stat_Ok);
-
-  // Enable any additional sensors / features
-  success &= (myICM.enableDMPSensor(INV_ICM20948_SENSOR_RAW_GYROSCOPE) == ICM_20948_Stat_Ok);
-  success &= (myICM.enableDMPSensor(INV_ICM20948_SENSOR_RAW_ACCELEROMETER) == ICM_20948_Stat_Ok);
-  success &= (myICM.enableDMPSensor(INV_ICM20948_SENSOR_MAGNETIC_FIELD_UNCALIBRATED) == ICM_20948_Stat_Ok);
-
-  // Configuring DMP to output data at multiple ODRs:
-  // DMP is capable of outputting multiple sensor data at different rates to FIFO.
-  // Setting value can be calculated as follows:
-  // Value = (DMP running rate / ODR ) - 1
-  // E.g. For a 5Hz ODR rate when DMP is running at 55Hz, value = (55/5) - 1 = 10.
-  success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Quat6, 55) == ICM_20948_Stat_Ok); // Set to the maximum
-  success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Accel, 55) == ICM_20948_Stat_Ok); // Set to the maximum
-  success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Gyro, 55) == ICM_20948_Stat_Ok); // Set to the maximum
-  success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Gyro_Calibr, 55) == ICM_20948_Stat_Ok); // Set to the maximum
-  success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Cpass, 55) == ICM_20948_Stat_Ok); // Set to the maximum
-  success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Cpass_Calibr, 55) == ICM_20948_Stat_Ok); // Set to the maximum
-
-  // Enable the FIFO
-  success &= (myICM.enableFIFO() == ICM_20948_Stat_Ok);
-
-  // Enable the DMP
-  success &= (myICM.enableDMP() == ICM_20948_Stat_Ok);
-
-  // Reset DMP
-  success &= (myICM.resetDMP() == ICM_20948_Stat_Ok);
-
-  // Reset FIFO
-  success &= (myICM.resetFIFO() == ICM_20948_Stat_Ok);
-
-  // Check success
-  if (success)
-  {
-  }
-  else
-  {
-   while (1)
-      ; // Do nothing more
-  }
-}
-
-unsigned int count = 0;
-
-void loop()
-{
-  icm_20948_DMP_data_t data;
-  myICM.readDMPdataFromFIFO(&data);
-
-  if ((myICM.status == ICM_20948_Stat_Ok) || (myICM.status == ICM_20948_Stat_FIFOMoreDataAvail)) // Was valid data available?
-  {
-
-    unsigned long currentTime = micros();
-    SERIAL_PORT.print(currentTime/1000000.0, 6); Serial.print(F(","));
-
-    if ((data.header & DMP_header_bitmap_Accel) > 0) { 
-      SERIAL_PORT.print(data.Raw_Accel.Data.X); Serial.print(F(","));
-      SERIAL_PORT.print(data.Raw_Accel.Data.Y); Serial.print(F(","));
-      SERIAL_PORT.print(data.Raw_Accel.Data.Z); Serial.print(F(","));
-    }
-
-    if((data.header & DMP_header_bitmap_Gyro) > 0) {
-      SERIAL_PORT.print(data.Raw_Gyro.Data.X); Serial.print(F(","));
-      SERIAL_PORT.print(data.Raw_Gyro.Data.Y); Serial.print(F(","));
-      SERIAL_PORT.print(data.Raw_Gyro.Data.Z); Serial.print(F(","));
-    }
-
-    if((data.header & DMP_header_bitmap_Compass) > 0){
-      SERIAL_PORT.print(data.Compass.Data.X); Serial.print(F(","));
-      SERIAL_PORT.print(data.Compass.Data.Y); Serial.print(F(","));
-      SERIAL_PORT.print(data.Compass.Data.Z); Serial.print(F(","));
-    }
-
-    if ((data.header & DMP_header_bitmap_Quat6) > 0) // We have asked for GRV data so we should receive Quat6
-    {
-      // Q0 value is computed from this equation: Q0^2 + Q1^2 + Q2^2 + Q3^2 = 1.
-      // In case of drift, the sum will not add to 1, therefore, quaternion data need to be corrected with right bias values.
-      // The quaternion data is scaled by 2^30.
-
-      // Scale to +/- 1
-      double q1 = ((double)data.Quat6.Data.Q1) / 1073741824.0; // Convert to double. Divide by 2^30
-      double q2 = ((double)data.Quat6.Data.Q2) / 1073741824.0; // Convert to double. Divide by 2^30
-      double q3 = ((double)data.Quat6.Data.Q3) / 1073741824.0; // Convert to double. Divide by 2^30
-
-      double q0 = sqrt(1.0 - ((q1 * q1) + (q2 * q2) + (q3 * q3)));
-
-      double qw = q0; // See issue #145 - thank you @Gord1
-      double qx = q2;
-      double qy = q1;
-      double qz = -q3;
-
-      SERIAL_PORT.print(qw); SERIAL_PORT.print(",");
-      SERIAL_PORT.print(qx); SERIAL_PORT.print(",");
-      SERIAL_PORT.print(qy); SERIAL_PORT.print(",");
-      SERIAL_PORT.print(qz);
-    }
-
-    Serial.println(F(""));
-  }
-
-  if (myICM.status != ICM_20948_Stat_FIFOMoreDataAvail) // If more data is available then we should read it right away - and not delay
-  {
-    delay(10);
-  }
-  
-  
-  delay(10000);
-
-  myICM.enableDMP(false);
-  delay(10000);
-
-  // Enable the DMP Game Rotation Vector sensor
-  myICM.enableDMPSensor(INV_ICM20948_SENSOR_GAME_ROTATION_VECTOR) == ICM_20948_Stat_Ok;
-
-  // Enable any additional sensors / features
-  myICM.enableDMPSensor(INV_ICM20948_SENSOR_RAW_GYROSCOPE) == ICM_20948_Stat_Ok;
-  myICM.enableDMPSensor(INV_ICM20948_SENSOR_RAW_ACCELEROMETER) == ICM_20948_Stat_Ok;
-  myICM.enableDMPSensor(INV_ICM20948_SENSOR_MAGNETIC_FIELD_UNCALIBRATED) == ICM_20948_Stat_Ok;
-
-  myICM.enableDMP();
-  myICM.resetFIFO();
-  myICM.resetDMP();
-  
-}*/
-
-/****************************************************************
  * Author: Lucía Alonso Mozo
  * Builds on Example7_DMP_Quat6_EulerAngles.ino
  * ICM 20948 Arduino Library Demo
@@ -320,12 +85,14 @@ void setup()
   success &= (myICM.enableDMPSensor(INV_ICM20948_SENSOR_MAGNETIC_FIELD_UNCALIBRATED) == ICM_20948_Stat_Ok);
 
   // Value = (DMP running rate / ODR ) - 1
-  success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Quat6, 55) == ICM_20948_Stat_Ok); // Set to the maximum
-  success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Accel, 55) == ICM_20948_Stat_Ok); // Set to the maximum
-  success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Gyro, 55) == ICM_20948_Stat_Ok); // Set to the maximum
-  success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Gyro_Calibr, 55) == ICM_20948_Stat_Ok); // Set to the maximum
-  success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Cpass, 55) == ICM_20948_Stat_Ok); // Set to the maximum
-  success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Cpass_Calibr,55) == ICM_20948_Stat_Ok); // Set to the maximum
+  // For high frequency, set the Value to 0
+  // For low frequency, set the Value to 55 (for example)
+  success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Quat6, 0) == ICM_20948_Stat_Ok); // Set to the maximum
+  success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Accel, 0) == ICM_20948_Stat_Ok); // Set to the maximum
+  success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Gyro, 0) == ICM_20948_Stat_Ok); // Set to the maximum
+  success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Gyro_Calibr, 0) == ICM_20948_Stat_Ok); // Set to the maximum
+  success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Cpass, 0) == ICM_20948_Stat_Ok); // Set to the maximum
+  success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Cpass_Calibr,0) == ICM_20948_Stat_Ok); // Set to the maximum
 
   // Enable the FIFO
   success &= (myICM.enableFIFO() == ICM_20948_Stat_Ok);
@@ -414,8 +181,9 @@ void loop()
     delay(10);
   }
 
-  delay(10000);
+  SERIAL_PORT.println(">>> Turning off the DMP");
 
+  delay(10000);
   myICM.enableDMP(false);
   delay(10000);
 
