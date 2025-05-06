b1098bab3271cf97662109e622bc714c882fab24
lualon09 <lualon09@ucm.es>
2025-05-06 13:05:54 +0200

    changing language to english, adding the off of the magnetometer and including the sleep of the BMP388

--- ICM_20948/Power_consumption/Power_consumption.ino
+++ ICM_20948/Power_consumption/Power_consumption.ino
@@ -12,8 +12,7 @@
 
 
 #include "ICM_20948.h" // Click here to get the library: http://librarymanager/All#SparkFun_ICM_20948_IMU
-
-//#define USE_SPI       // Uncomment this to use SPI
+#include <BMP388_DEV.h> 
 
 #define SERIAL_PORT Serial
 
@@ -29,13 +28,9 @@
 #define WIRE_PORT Wire // Your desired Wire port.      Used when "USE_SPI" is not defined
 // The value of the last bit of the I2C address.
 // On the SparkFun 9DoF IMU breakout the default is 1, and when the ADR jumper is closed the value becomes 0
-#define AD0_VAL 1
+#define AD0_VAL 0
 
-#ifdef USE_SPI
-ICM_20948_SPI myICM; // If using SPI create an ICM_20948_SPI object
-#else
 ICM_20948_I2C myICM; // Otherwise create an ICM_20948_I2C object
-#endif
 
 void setup()
 {
@@ -45,32 +40,28 @@ void setup()
   {
   };
 
-#ifdef USE_SPI
-  SPI_PORT.begin();
-#else
-
   WIRE_PORT.setPins(SDA_PIN,SCL_PIN); 
   pinMode(REG_1V8_EN_PIN, OUTPUT);
   digitalWrite(REG_1V8_EN_PIN, 1);
   delay(100);
   WIRE_PORT.begin();
   WIRE_PORT.setClock(400000);
-#endif
+  BMP388_DEV bmp388;   
+  bmp388.begin(SLEEP_MODE, 0x76);
 
-  //myICM.enableDebugging(); // Uncomment this line to enable helpful debug messages on Serial
+  // myICM.enableDebugging(); // Uncomment this line to enable helpful debug messages on Serial
 
   bool initialized = false;
   while (!initialized)
   {
 
-#ifdef USE_SPI
-    myICM.begin(CS_PIN, SPI_PORT, SPI_FREQ); // Here we are using the user-defined SPI_FREQ as the clock speed of the SPI bus
-#else
     myICM.begin(WIRE_PORT, AD0_VAL);
-#endif
 
+    SERIAL_PORT.print(F("Initialization of the sensor returned: "));
+    SERIAL_PORT.println(myICM.statusString());
     if (myICM.status != ICM_20948_Stat_Ok)
     {
+      SERIAL_PORT.println("Trying again...");
       delay(500);
     }
     else
@@ -79,20 +70,28 @@ void setup()
     }
   }
 
+  // In this advanced example we'll cover how to do a more fine-grained setup of your sensor
+  SERIAL_PORT.println("Device connected!");
+
   // Here we are doing a SW reset to make sure the device starts in a known state
   myICM.swReset();
+  if (myICM.status != ICM_20948_Stat_Ok)
+  {
+    SERIAL_PORT.print(F("Software Reset returned: "));
+    SERIAL_PORT.println(myICM.statusString());
+  }
   delay(250);
 
   // Now wake the sensor up
   myICM.sleep(false);
   myICM.lowPower(false);
 
-  // The next few configuration functions accept a bit-mask of sensors for which the settings should be applied.
-
-  // Set Gyro and Accelerometer to a particular sample mode
-  // options: ICM_20948_Sample_Mode_Continuous
-  //          ICM_20948_Sample_Mode_Cycled
   myICM.setSampleMode((ICM_20948_Internal_Acc | ICM_20948_Internal_Gyr), ICM_20948_Sample_Mode_Continuous);
+  if (myICM.status != ICM_20948_Stat_Ok)
+  {
+    SERIAL_PORT.print(F("setSampleMode returned: "));
+    SERIAL_PORT.println(myICM.statusString());
+  }
   
   // Set full scale ranges for both acc and gyr
   ICM_20948_fss_t myFSS; // This uses a "Full Scale Settings" structure that can contain values for all configurable sensors
@@ -110,6 +109,11 @@ void setup()
                     // dps2000
 
   myICM.setFullScale((ICM_20948_Internal_Acc | ICM_20948_Internal_Gyr), myFSS);
+  if (myICM.status != ICM_20948_Stat_Ok)
+  {
+    SERIAL_PORT.print(F("setFullScale returned: "));
+    SERIAL_PORT.println(myICM.statusString());
+  }
 
   // Set up Digital Low-Pass Filter configuration
   ICM_20948_dlpcfg_t myDLPcfg;    // Similar to FSS, this uses a configuration structure for the desired sensors
@@ -134,51 +138,147 @@ void setup()
 
   myICM.setDLPFcfg((ICM_20948_Internal_Acc | ICM_20948_Internal_Gyr), myDLPcfg);
 
-  // Choose whether or not to use DLPF
+  if (myICM.status != ICM_20948_Stat_Ok)
+  {
+    SERIAL_PORT.print(F("setDLPcfg returned: "));
+    SERIAL_PORT.println(myICM.statusString());
+  }
+
   ICM_20948_Status_e accDLPEnableStat = myICM.enableDLPF(ICM_20948_Internal_Acc, true);
   ICM_20948_Status_e gyrDLPEnableStat = myICM.enableDLPF(ICM_20948_Internal_Gyr, true);
+  SERIAL_PORT.print(F("Enable DLPF for Accelerometer returned: "));
+  SERIAL_PORT.println(myICM.statusString(accDLPEnableStat));
+  SERIAL_PORT.print(F("Enable DLPF for Gyroscope returned: "));
+  SERIAL_PORT.println(myICM.statusString(gyrDLPEnableStat));
   
-
-  // Choose whether or not to start the magnetometer
   myICM.startupMagnetometer();
-  myICM.setSampleMode((ICM_20948_Internal_Acc | ICM_20948_Internal_Gyr), ICM_20948_Sample_Mode_Continuous);
+  if (myICM.status != ICM_20948_Stat_Ok)
+  {
+    SERIAL_PORT.print(F("startupMagnetometer returned: "));
+    SERIAL_PORT.println(myICM.statusString());
+  }
 
+  SERIAL_PORT.println();
+  SERIAL_PORT.println(F("Configuration complete!"));
 }
 
 void loop()
 {
-
-  // Frecuencia alta: 400 Hz (real ≈ 562.5 Hz)
-  
-  // SERIAL_PORT.println(">>> Capturando datos a ~400Hz");
+  // High frequency: 400 Hz
+  SERIAL_PORT.println(">>> Capturing data at ~400Hz");
   ICM_20948_smplrt_t mySmplrtHigh;
-  mySmplrtHigh.g = 1;  // 1125 / (1 + 1) = 562.5 Hz
+  mySmplrtHigh.g = 1;  
   mySmplrtHigh.a = 1;
   myICM.setSampleRate((ICM_20948_Internal_Acc | ICM_20948_Internal_Gyr), mySmplrtHigh);
-  delay(10000);
+  delay(1000);
 
-  // SERIAL_PORT.println(">>> Espera antes de cambiar a ~20Hz");
-  // delay(5000);
+  unsigned long startTime = millis();
+  while (millis() - startTime < 5000) {  // 5 seconds
+    if (myICM.dataReady()) {
+      myICM.getAGMT();
+      unsigned long actual = micros();
+      printScaledAGMT(&myICM, actual);
+    }
+  }
 
-  // Frecuencia baja: 20 Hz (real ≈ 20.4 Hz)
-  //SERIAL_PORT.println(">>> Capturando datos a ~20Hz");
+  SERIAL_PORT.println(">>> Waiting 5 seconds before changing to ~20Hz");
+  delay(5000);
+
+  // Low frequency
+  SERIAL_PORT.println(">>> Capturing data at ~20Hz");
   ICM_20948_smplrt_t mySmplrtLow;
-  mySmplrtLow.g = 55;  // 1125 / (55 + 1) = 20.1 Hz
+  mySmplrtLow.g = 55;
   mySmplrtLow.a = 55;
   myICM.setSampleRate((ICM_20948_Internal_Acc | ICM_20948_Internal_Gyr), mySmplrtLow);
-  delay(10000);
-  
-  
-  // SERIAL_PORT.println(">>> Activando modo dormido");
-  myICM.sleep(true);
-  delay(10000); // En este tiempo aunque le pidieramos al sensor, no nos da nada.
-  myICM.sleep(false);
+  delay(1000);
 
-  // SERIAL_PORT.println(">>> Activando modo bajo consumo");
-  // Modo de bajo consumo
+  startTime = millis();
+  while (millis() - startTime < 5000) {  // 5 seconds
+    if (myICM.dataReady()) {
+      myICM.getAGMT();
+      unsigned long actual = micros();
+      printScaledAGMT(&myICM, actual);
+    }
+  }
+
+  SERIAL_PORT.println(">>> Activating sleep mode");
+  delay(100);
+  myICM.resetMag();
+  ICM_20948_Status_e stat = myICM.sleep(true);
+  Serial.print(stat);
+  delay(5000);
+  myICM.sleep(false);
+  myICM.startupMagnetometer();
+
+
+  SERIAL_PORT.println(">>> Activating low power mode");
   myICM.lowPower(true);
-  delay(10000);
+  startTime = millis();
+  while (millis() - startTime < 5000) {  // 5 seconds
+    if (myICM.dataReady()) {
+      myICM.getAGMT();
+      unsigned long actual = micros();
+      printScaledAGMT(&myICM, actual);
+    }
+  }
   myICM.lowPower(false);
 
+  SERIAL_PORT.println(">>> Full cycle, restarting in 5 seconds...");
+  delay(5000);
 }
 
+// Below here are some helper functions to print the data nicely!
+
+void printFormattedFloat(float val, uint8_t leading, uint8_t decimals)
+{
+  float aval = abs(val);
+  if (val < 0)
+  {
+    SERIAL_PORT.print("-");
+  }
+  for (uint8_t indi = 0; indi < leading; indi++)
+  {
+    uint32_t tenpow = 0;
+    if (indi < (leading - 1))
+    {
+      tenpow = 1;
+    }
+    for (uint8_t c = 0; c < (leading - 1 - indi); c++)
+    {
+      tenpow *= 10;
+    }
+    break;
+  }
+  if (val < 0)
+  {
+    SERIAL_PORT.print(-val, decimals);
+  }
+  else
+  {
+    SERIAL_PORT.print(val, decimals);
+  }
+}
+
+void printScaledAGMT(ICM_20948_I2C *sensor, unsigned long time)
+{
+  printFormattedFloat(time/1000000.0, 2, 6);
+  SERIAL_PORT.print(",");
+  printFormattedFloat(sensor->accX(), 1, 5);
+  SERIAL_PORT.print(",");
+  printFormattedFloat(sensor->accY(), 1, 5);
+  SERIAL_PORT.print(",");
+  printFormattedFloat(sensor->accZ(), 1, 5);
+  SERIAL_PORT.print(",");
+  printFormattedFloat(sensor->gyrX(), 3, 5);
+  SERIAL_PORT.print(",");
+  printFormattedFloat(sensor->gyrY(), 3, 5);
+  SERIAL_PORT.print(",");
+  printFormattedFloat(sensor->gyrZ(), 3, 5);
+  SERIAL_PORT.print(",");
+  printFormattedFloat(sensor->magX(),3, 2);
+  SERIAL_PORT.print(",");
+  printFormattedFloat(sensor->magY(), 3, 2);
+  SERIAL_PORT.print(",");
+  printFormattedFloat(sensor->magZ(), 3, 2); 
+  SERIAL_PORT.println();
+} 
